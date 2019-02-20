c7959ef305f4dc66f3c4e44a1b736e31b689a3bc
nirav dhanorkar <nirav dhanorkar>
2019-02-21 01:53:29 +0530

    dynamic graph

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnChanges, SimpleChanges } from "@angular/core";
 import { Chart } from "chart.js";
 
 @Component({
@@ -6,26 +6,59 @@ import { Chart } from "chart.js";
   templateUrl: "./about.component.html",
   styleUrls: ["./about.component.scss"]
 })
-export class AboutComponent implements OnInit {
-  LineChart = [];
-  pieChart = [];
-  barChart = [];
-
+export class AboutComponent implements OnInit{
+  object: any = {}
   title: string = "chart";
-  window: Window;
 
+  noOfTotalTypesOfRoom: number = 3
+  typesOfRoom = ["Standard", "Delux", "Super Delux"]
+  totalNoOfRooms = [5,5,5]
+  occupiedRooms = [4,3,2]
+  priceOfEachTypeRoom = [5000, 10000, 15000]
+ 
   constructor() {}
 
+  generateGraph(){
+    this.object.LineChart.update()
+    this.object.pieChart1.update()
+    this.object.barChart.update()
+    this.object.pieChart2.update()    
+  }
+  
+
+  getArray(noOfTimes:number): any{
+    let arrRooms = []
+    for(let i=0;i<noOfTimes;i++){
+      arrRooms.push(i);
+    }
+    return arrRooms;
+  }
+
+  getCurrentIncome(){
+    let arr =[]
+    arr = this.priceOfEachTypeRoom.map((obj, index) => {
+      return obj * this.occupiedRooms[index]
+    })    
+    return arr;    
+  }
+  getUpxsellingIncome(){
+    let arr =[]
+    arr = this.priceOfEachTypeRoom.map((obj, index) => {
+      return obj * this.totalNoOfRooms[index]
+    })    
+    return arr;    
+  }  
+
   ngOnInit() {
-    this.LineChart = new Chart("lineChart", {
+    this.object.LineChart = new Chart("lineChart", {
       type: "line",
       data: {
-        labels: ["Standard", "Delux", "Super Delux"],
+        labels: this.typesOfRoom,
         datasets: [
           {
             label: "Currrent case",
             fill: true,
-            data: [25000, 30000, 30000],
+            data: this.getCurrentIncome(),
             lineTension: 0.2,
             borderColor: "blue",
             borderWidth: 1
@@ -33,7 +66,7 @@ export class AboutComponent implements OnInit {
           {
             label: "Upxselling technology",
             fill: true,
-            data: [25000, 46000, 66000],
+            data: this.getUpxsellingIncome(),
             lineTension: 0.2,
             borderColor: "green",
             borderWidth: 1
@@ -61,12 +94,21 @@ export class AboutComponent implements OnInit {
       }
     });
 
-    this.pieChart = new Chart("pieChart-1", {
+    this.object.pieChart1 = new Chart("pieChart-1", {
       type: "pie",
       data: {
         datasets: [
           {
-            data: [137000, 85000],
+            // data: [137000, 85000],
+            data: [
+                this.getUpxsellingIncome().reduce((accumulator, a) => {
+                  return accumulator + a
+                })
+              ,
+                this.getCurrentIncome().reduce((accumulator, a) => {
+                  return accumulator + a
+                })
+              ],
             backgroundColor: ["green", "blue"],
             label: "Dataset 1"
           }
@@ -82,22 +124,22 @@ export class AboutComponent implements OnInit {
       }
     });
 
-    this.barChart = new Chart("barChart", {
+    this.object.barChart = new Chart("barChart", {
       type: "bar",
       data: {
-        labels: ["Standard", "Delux", "Super Delux"],
+        labels: this.typesOfRoom,
         datasets: [
           {
             label: "Current Case",
             backgroundColor: "blue",
             yAxisID: "y-axis-1",
-            data: [5, 3, 2]
+            data: this.occupiedRooms
           },
           {
             label: "Upxselling Technology",
             backgroundColor: "green",
             yAxisID: "y-axis-2",
-            data: [5, 5, 5]
+            data: this.totalNoOfRooms
           }
         ]
       },
@@ -139,12 +181,20 @@ export class AboutComponent implements OnInit {
       }
     });
 
-    this.pieChart = new Chart("pieChart-2", {
+    this.object.pieChart2 = new Chart("pieChart-2", {
       type: "pie",
       data: {
         datasets: [
           {
-            data: [15, 10],
+            data: [
+              this.totalNoOfRooms.reduce((accumulator, a) => {
+                return accumulator + a
+              })
+            ,
+              this.occupiedRooms.reduce((accumulator, a) => {
+                return accumulator + a
+              })
+            ],
             backgroundColor: ["green", "blue"],
             label: "Rooms"
           }
