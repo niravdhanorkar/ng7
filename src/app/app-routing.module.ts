de0789a9871daa107da5c6b783e5518f80f9208c
nirav dhanorkar <nirav dhanorkar>
2019-03-21 12:45:29 +0530

    image upload demo added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { ImageComponent } from './image/image.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'image', component: ImageComponent},
   {path: 'contact', component: ContactComponent},
 ];
 
